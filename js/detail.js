9843e7555ec4706d371be608d06926675cab6d29
Yongmin0423 <yym21345@gmail.com>
2025-03-08 02:15:42 +0900

    fix: bug

--- js/detail.js
+++ js/detail.js
@@ -1,14 +1,10 @@
 const API_KEY = "537786cf19164215ba386fb47bd70c9c"; // API 키
 
-const gameId = "4291"; // 게임 ID
-let url = new URL(`https://api.rawg.io/api/games/${gameId}?key=${API_KEY}`); // url 주소
-
 let gameData = []; // 게임 데이터를 저장할 변수
 let gameTags = []; // 해당 게임의 태그 데이터를 저장할 변수
 let publisherData = []; // 게임의 게시자 데이터를 저장할 변수
 let creatorData = []; // 게임의 크리에이터 데이터를 저장할 변수
 let redditData = [];
-// const gameId = "4291"; // 게임 ID
 
 // URL에서 gameId 가져오기
 const getGameIdFromURL = () => {
